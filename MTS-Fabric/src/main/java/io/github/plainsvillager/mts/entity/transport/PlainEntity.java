2abcf0fb0938d2ad85a6ee8b6d7302ba00f594f2
HlHillMC <Qiuyixuan_last@outlook.com>
2023-02-06 20:44:30 +0800

    Update PlainEntity.java

--- MTS-Fabric/src/main/java/io/github/plainsvillager/mts/entity/transport/PlainEntity.java
+++ MTS-Fabric/src/main/java/io/github/plainsvillager/mts/entity/transport/PlainEntity.java
@@ -34,42 +34,28 @@ public class PlainEntity extends Entity {
         this.travel();
     }
 
-    private void updateVelocity() {
-        double d = -0.03999999910593033;
-        double e = this.hasNoGravity() ? 0.0 : -0.03999999910593033;
-        double f = 0.0;
-        float velocityDecay = 0.05F;
-        Vec3d vec3d = this.getVelocity();
-        this.setVelocity(vec3d.x * (double)velocityDecay, vec3d.y + e, vec3d.z * (double)velocityDecay);
-        if (f > 0.0) {
-            Vec3d vec3d2 = this.getVelocity();
-            this.setVelocity(vec3d2.x, (vec3d2.y + f * 0.06153846016296973) * 0.75, vec3d2.z);
-        }
-    }
-
     public void travel() {
         LivingEntity livingEntity = (LivingEntity) this.getFirstPassenger();
         if (this.hasPassengers() && livingEntity != null) {
             this.setRotation(livingEntity.getYaw(), livingEntity.getPitch() * 0.5F);
-            //float f = livingEntity.sidewaysSpeed * 0.5F;
+            float f = livingEntity.sidewaysSpeed * 0.5F;
             this.speed = 5;
-            float g = this.speed;//livingEntity.forwardSpeed;
+            float g = livingEntity.forwardSpeed;
             if (g <= 0.0F) {
                 g *= 0.25F;
             }
-
+ 
             if (livingEntity instanceof PlayerEntity) {
-                float yaw = livingEntity.getYaw();
-                float pitch = livingEntity.getPitch();
-                float x = g * MathHelper.sin(yaw) * MathHelper.cos(pitch);
+                float yaw = livingEntity.getYaw()/180*MathHelper.PI;
+                float pitch = -livingEntity.getPitch()/180*MathHelper.PI;
+                float x = - g * MathHelper.sin(yaw) * MathHelper.cos(pitch);
                 float y = g * MathHelper.sin(pitch);
                 float z = g * MathHelper.cos(yaw) * MathHelper.cos(pitch);
-                //this.updateVelocity();
-                //logger.info(g +";"+ x +";"+ y +";"+ z);
                 this.move(MovementType.SELF, new Vec3d(x, y, z));
             }
         }
     }
+
 
     @Override
     public void pushAwayFrom(Entity entity) {
